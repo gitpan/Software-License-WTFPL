f3e6d54037be92b0bda0c31f340aed5e51093cfd
Florian Ragwitz <rafl@debian.org>
2010-05-07 02:05:52 +0000

    Import of FLORA/Software-License-WTFPL-0.03 from CPAN.
    gitpan-cpan-distribution: Software-License-WTFPL
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         FLORA/Software-License-WTFPL-0.03.tar.gz
    gitpan-cpan-author:       FLORA
    gitpan-cpan-maturity:     released

--- lib/Software/License/WTFPL_2.pm
+++ lib/Software/License/WTFPL_2.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Software::License::WTFPL_2;
 BEGIN {
-  $Software::License::WTFPL_2::VERSION = '0.02';
+  $Software::License::WTFPL_2::VERSION = '0.03';
 }
 # ABSTRACT: The Do What The Fuck You Want To Public License, Version 2
 
@@ -23,10 +23,6 @@ sub meta_name { 'unrestricted' }
 =head1 NAME
 
 Software::License::WTFPL_2 - The Do What The Fuck You Want To Public License, Version 2
-
-=head1 VERSION
-
-version 0.02
 
 =head1 DESCRIPTION
 
@@ -66,11 +62,11 @@ such, it only covers copying, distribution and modification. If you
 want to add a no warranty clause for a program, you may use the
 following wording in your source code:
 
-/* This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details. */
+  /* This program is free software. It comes without any warranty, to
+   * the extent permitted by applicable law. You can redistribute it
+   * and/or modify it under the terms of the Do What The Fuck You Want
+   * To Public License, Version 2, as published by Sam Hocevar. See
+   * http://sam.zoy.org/wtfpl/COPYING for more details. */
 
 =item Isn’t this license basically public domain?
 
